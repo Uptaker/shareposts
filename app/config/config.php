44b9eff0c5194cc888d1f64c40fd3ffa03864d33
Uptaker <markus.tammeoja@gmail.com>
2021-09-01 22:24:50 +0300

    fix typo

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvctest');
+define('DB_NAME', 'shareposts');
 
 // App root
 define('APPROOT', dirname(dirname(__FILE__)));
