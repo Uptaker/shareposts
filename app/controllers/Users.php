3f381ca4557dc50541adfecf97f946e1d48a6b91
Uptaker <markus.tammeoja@gmail.com>
2021-09-01 23:27:00 +0300

    Add user authentication (no session)

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -120,9 +120,27 @@ class Users extends Controller
                 $data['password_err'] = 'Please enter your password';
             }
 
+            // Check for user/email
+            if ($this->userModel->findUserByEmail($data['email'])) {
+                // User found
+            } else {
+                $data['email_err'] = "No user with corresponding email found";
+            }
+
             // Make sure errors are empty
             if (empty($data['email_err']) && empty($data['password_err'])) {
-                die('success');
+                // Validated
+
+                // Check and set logged in user
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+                if ($loggedInUser) {
+                    // Create session
+                    die("logged in!");
+                } else {
+                    $data['password_err'] = 'Password incorrect';
+                    $this->view('users/login', $data);
+                }
             } else {
                 // Load view with errors
                 $this->view('users/login', $data);
--- app/models/User.php
+++ app/models/User.php
@@ -46,4 +46,20 @@ class User
             return false;
         }
     }
+
+    public function login($email, $password)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
+        $this->db->query($sql);
+        $this->db->bind('email', $email);
+
+        $row = $this->db->single();
+
+        $hashed_password = $row->password;
+        if (password_verify($password, $hashed_password)) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
 }
