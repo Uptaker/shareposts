768c8ee8796e238f36c30319e0836c907f128d56
Uptaker <markus.tammeoja@gmail.com>
2021-09-01 22:44:57 +0300

    add user registration

--- app/models/User.php
+++ app/models/User.php
@@ -14,13 +14,33 @@ class User
     {
         $sql = "SELECT * FROM users WHERE email = :email";
         $this->db->query($sql);
+
+        // Bind value
         $this->db->bind(':email', $email);
 
+        // Return single row
         $this->db->single();
 
         // Check row;
-
         if ($this->db->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Register user
+    public function register($data)
+    {
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $this->db->query($sql);
+
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':email', $data['email']);
+
+        if ($this->db->execute()) {
             return true;
         } else {
             return false;
