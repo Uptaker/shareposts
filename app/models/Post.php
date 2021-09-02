80fe8fb06acf3dc8f3db6f19bc582f7ced39065b
Uptaker <markus.tammeoja@gmail.com>
2021-09-02 16:05:08 +0300

    add post creation

--- app/models/Post.php
+++ app/models/Post.php
@@ -24,4 +24,21 @@ class Post
 
         return $this->db->resultSet();
     }
+
+    public function addPost($data)
+    {
+        $sql = "INSERT INTO posts (title, user_id, body) VALUES (:title, :user_id, :body)";
+        $this->db->query($sql);
+
+        // Bind values
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':body', $data['body']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
